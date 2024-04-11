118628f83301021599a70145b539ec74b5aecf5c
derpylama <larssonemil55@gmail.com>
2024-04-11 15:03:51 +0200

    started with the admin page

--- php/admin.php
+++ php/admin.php
@@ -4,8 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
-    
+    <main>
+        <nav>    
+            <form action="" id = "dropDown">
+                <select name="deltavling" id="deltavling">
+                    <option value="deltavling1">Deltävling 1</option>
+                    <option value="deltavling2">Deltävling 2</option>
+                    <option value="deltavling3">Deltävling 3</option>
+                    <option value="deltavling4">Deltävling 4</option>
+                </select>
+            </form>
+        </nav>
+
+        <section>
+            <form action="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+
+
+            </form>
+        </section>
+
+
+
+
+    </main>
 </body>
 </html>
